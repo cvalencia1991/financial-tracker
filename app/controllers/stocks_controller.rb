5edf6b1f5159f884882f1ddbdcd70d47c32eb73c
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-02 12:17:02 -0500

    Implement Ajax in ruby on rails

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,17 @@
+# class Stocks Controller
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render json: @stock, status: :ok
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = 'Please enter a valid symbol to search'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = 'Please enter a symbol to search'
+      redirect_to my_portfolio_path
+    end
   end
 end
