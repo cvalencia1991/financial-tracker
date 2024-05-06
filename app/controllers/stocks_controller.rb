1b13018bc76c508218a394bfa5767ff9e22eeca5
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-06 16:18:59 -0500

    Implement TurboStream using Ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,14 +4,26 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.turbo_stream do
+             render turbo_stream: turbo_stream.update(
+            'results',
+             partial: 'users/result'
+            )
+           end
+        end
       else
-        flash[:alert] = 'Please enter a valid symbol to search'
-        redirect_to my_portfolio_path
+        flash_message_symbol
       end
     else
-      flash[:alert] = 'Please enter a symbol to search'
-      redirect_to my_portfolio_path
+      flash_message_symbol
     end
+  end
+
+  private
+
+  def flash_message_symbol
+    flash[:alert] = 'Please enter a valid symbol to search'
+    redirect_to my_portfolio_path
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,9 +5,9 @@ class Stock < ApplicationRecord
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
       secret_token: 'sk_7db5bce69e214273a0c8325b67e93712',
       endpoint: 'https://cloud.iexapis.com/v1'
-   )
+    )
     begin
-     new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
       return nil
     end
