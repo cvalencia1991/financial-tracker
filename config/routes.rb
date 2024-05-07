f135a5b7a49464cacbfa6de0fa7f97eba95006b3
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-07 16:42:24 -0500

    add functionaly to remove tracker

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   get 'home/index'
   get 'up' => 'rails/health#show', as: :rails_health_check
