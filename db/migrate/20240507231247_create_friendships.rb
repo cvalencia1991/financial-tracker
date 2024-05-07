b08a3c2ec8e348b4af7a3a1a63a71c95ca1e32d8
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-07 18:36:56 -0500

    Implement self reference association

---
+++ db/migrate/20240507231247_create_friendships.rb
@@ -0,0 +1,9 @@
+class CreateFriendships < ActiveRecord::Migration[7.1]
+  def change
+    create_table :friendships do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :friend, null: false, foreign_key: { to_table: :users }
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_214048) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_231247) do
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
     t.string "name"
@@ -42,6 +51,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_214048) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "user_stocks", "stocks"
   add_foreign_key "user_stocks", "users"
 end
