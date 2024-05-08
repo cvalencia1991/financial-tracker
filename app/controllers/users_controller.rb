8de10438b0360d546dba53038d53c448ad2d184f
cvalencia1991 <cesar4a6z@gmail.com>
2024-05-08 17:09:38 -0500

    "upgrade tracking Options"
    - features tracking options to the design
    - Implements create and destroy paths of friendships
    - Show Friend in the show Route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,15 @@
 class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
+    @user = current_user
   end
   def my_friends
     @friends = current_user.friends
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
   def search
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,7 +7,7 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-      secret_token: 'sk_7db5bce69e214273a0c8325b67e93712',
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
     begin
